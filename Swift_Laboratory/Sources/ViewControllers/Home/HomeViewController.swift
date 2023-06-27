dc40217bea9cb16094e45dc68981f57e8273393c
Masaya1582 <masa8y88@gmail.com>
2023-06-27 22:05:22 +0900

    feat: Rebaseテスト

--- Swift_Laboratory/Sources/ViewControllers/Home/HomeViewController.swift
+++ Swift_Laboratory/Sources/ViewControllers/Home/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World")
+        print("Hello Swift")
     }
 
 }
